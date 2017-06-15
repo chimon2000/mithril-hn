991ee36fd909ccbf85cfa0c5de98be59d73c08bf
Ryan <ryan.edge@zoho.com>
2017-06-15 00:52:40 -0400

    Added loading to itemview

--- src/views/ItemView.ts
+++ src/views/ItemView.ts
@@ -5,6 +5,7 @@ import { white, em, px } from 'csx'
 import { ClassComponent } from 'mithril'
 import News from '../models/News'
 import CommentItem from '../comments/comment-item'
+import Loading from '../components/loading'
 
 const article = style({
     backgroundColor: white.toHexString(),
@@ -31,27 +32,36 @@ const unstyled = style({
     textDecoration: 'none'
 })
 
+const noComments = style({
+    marginTop: px(10)
+})
+
 const unstyledLink = classes(link, unstyled)
+
+const comments = comments => {
+    return comments.length > 0
+        ? comments.map(comment => m(CommentItem, { comment }))
+        : m('div', { className: noComments }, 'No comments yet')
+}
 
 export default class ItemView implements ClassComponent<{}> {
     oninit = async vnode => News.load({ id: vnode.attrs.id })
 
     view() {
         let { current: story } = News
+        let { current: loading } = News.loading
 
-        return m('article', { className: article }, [
-            m('a', { className: unstyledLink, href: story.url }, story.title),
-            m('div', { className: detail }, [
-                m('span', `${story.points} points by `),
-                m('a', { href: `/user/${story.user}`, oncreate: m.route.link }, story.user),
-                m('span', ` ${story.time_ago} | `),
-                m('span', {}, `${story.comments_count} comments`)
-            ]),
-            m(
-                'div',
-                { className: comment },
-                story.comments && story.comments.map(comment => m(CommentItem, { comment }))
-            )
-        ])
+        return loading
+            ? m(Loading)
+            : m('article', { className: article }, [
+                  m('a', { className: unstyledLink, href: story.url }, story.title),
+                  m('div', { className: detail }, [
+                      m('span', `${story.points} points by `),
+                      m('a', { href: `/user/${story.user}`, oncreate: m.route.link }, story.user),
+                      m('span', ` ${story.time_ago} | `),
+                      m('span', {}, `${story.comments_count} comments`)
+                  ]),
+                  m('div', { className: comment }, story.comments && comments(story.comments))
+              ])
     }
 }
