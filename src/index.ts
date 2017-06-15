cf49d83bd4726d824779edfa41f5f2e9e91fd956
Ryan <ryan.edge@zoho.com>
2017-06-15 00:50:56 -0400

    remove unused code

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,10 @@ import * as m from 'mithril'
 
 import routes from './routes'
 
-import './styles/main.css'
-
-import { normalize, setupPage } from 'csstips'
+import { normalize } from 'csstips'
 
 normalize()
 
 const root = document.body
 
-const welcome = m('h1', { class: 'title' }, 'My first app')
-const button = m('button', 'A button')
-const Home = m('main', [welcome, button])
-
-m.route(root, '/top', routes)
+m.route(root, '/top/1', routes)
